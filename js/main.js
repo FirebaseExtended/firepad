c733dcfd8059798de131d386880a928b8be84d14
Michael Lehenbauer <mikelehen@gmail.com>
2013-04-06 22:32:43 -0700

    Tweak sharding.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 var ROOMS = 20;
-var ROOMS_PER_NS = 10;
 
 var room = Number(window.location.hash.replace(/#/g, ''));
 if (!(room >= 1 && room <= ROOMS)) {
   room = Math.floor(Math.random() * ROOMS);
 }
-var firebaseUrl = 'https://firebase-firepad' + Math.floor(room / ROOMS_PER_NS) + '.firebaseio.com/' + (room % ROOMS_PER_NS);
 
 window.location = window.location.toString().replace(/#.*/, '') + '#' + room;
 
-var firebaseUrl = 'https://firebase-firepad' + Math.floor(room / ROOMS_PER_NS) + '.firebaseio.com/' + (room % ROOMS_PER_NS);
+var NAMESPACES = 15;
+var firebaseUrl = 'https://firebase-firepad' + (room % NAMESPACES) + '.firebaseio.com/' + room;
 var firepadRef = new Firebase(firebaseUrl);
 var codeMirror = CodeMirror(document.getElementById('firepad'), { lineWrapping: true });
 
