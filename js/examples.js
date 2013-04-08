a155cf34aca637ab3a9c598a5a85eeafdf0beaa3
Michael Lehenbauer <mikelehen@gmail.com>
2013-04-08 10:53:16 -0700

    Remove embedded example code.

--- js/examples.js
+++ js/examples.js
@@ -1,6 +1,5 @@
 var examples = ['richtext', 'code', 'userlist'];
 var GITHUB_BASE_URL = 'https://github.com/firebase/firepad/blob/gh-pages/examples/';
-var examplesLoaded = 0;
 
 function main() {
   for(var i = 0; i < examples.length; i++) {
@@ -12,11 +11,4 @@ setTimeout(main, 0);
 function loadExample(example) {
   var githubUrl = GITHUB_BASE_URL + example + '.html';
   $('#' + example + ' .example-view-source').attr('href', githubUrl);
-  $.ajax({ url: example + '.html' }).done(function(data) {
-    $('#' + example + ' .example-code').text(data);
-    examplesLoaded++;
-    if (examplesLoaded === examples.length) {
-      prettyPrint();
-    }
-  });
 }
