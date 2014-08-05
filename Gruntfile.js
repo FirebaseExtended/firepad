ccf06863cb4c3a9891fc3ab6d3bd44f1d549ba9b
Michael Lehenbauer <michael@firebase.com>
2014-08-05 12:39:23 -0700

    Make examples more standalone (copy/pastable).
    * Pull firepad from CDN.
    * Remove example-helper.js script include.
    * Clean up comments slightly.

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,12 +99,6 @@ module.exports = function (grunt) {
             src: 'lib/firepad.css',
             dest: 'dist/firepad.css'
           },
-          {
-            expand: true,
-            cwd: 'dist/',
-            src: ['firepad.js', 'firepad.css'],
-            dest: 'examples/'
-          }
         ]
       }
     },
