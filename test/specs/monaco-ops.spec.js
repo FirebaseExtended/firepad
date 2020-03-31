072faf423ef022aeb7f8cc2c334a87072e9219ce
Brijesh Bittu <brijeshb42@gmail.com>
2020-03-31 21:03:03 +0530

    Refactor monaco change to ot.js change calculation
    * Supports multiple changes in the same batch

--- test/specs/monaco-ops.spec.js
+++ test/specs/monaco-ops.spec.js
@@ -17,38 +17,51 @@ describe('Monaco Operations Test', function () {
     }
 }
 `;
+    class Range {
+      constructor(startLineNumber, startColumn, endLineNumber, endColumn) {
+        this.startLineNumber = startLineNumber;
+        this.startColumn = startColumn;
+        this.endLineNumber = endLineNumber;
+        this.endColumn = endColumn;
+      }
+
+      isEmpty() {
+        return this.startLineNumber === this.endLineNumber && this.startColumn === this.endColumn;
+      }
+    }
+
 
     /** Editor Changes */
     var operations = [
-        { rangeLength: 0, text: ' */', rangeOffset: 299, forceMoveMarkers: false },
-        { rangeLength: 0, text: '/* ', rangeOffset: 21, forceMoveMarkers: false }
+      { rangeLength: 0, text: ' */', rangeOffset: 302, range: new Range(15, 1, 15, 1), forceMoveMarkers: false },
+      { rangeLength: 0, text: '/* ', rangeOffset: 0, range: new Range(1, 1, 1, 1), forceMoveMarkers: false }
     ];
 
-    /** Expected Text Operations */
-    var textOperations = [
-        new firepad.TextOperation().retain(21).insert('/* ').retain(281),
-        new firepad.TextOperation().retain(302).insert(' */').retain(3)
-    ];
+    window.monaco = {
+      Range,
+    };
 
     it('should convert Monaco Editor changes to Text Operation', function () {
         var MonacoAdapter = firepad.MonacoAdapter;
-        var operationFromMonacoChange = MonacoAdapter.prototype.operationFromMonacoChanges;
+        var operationFromMonacoChange = MonacoAdapter.prototype.operationFromMonacoChange;
+        var context = {
+          lastDocLines: editorContent.split('\n'),
+          getModel() {
+            return {
+              getEOL() {
+                return '\n';
+              },
+            };
+          },
+        };
+        var pair = operationFromMonacoChange.call(context, operations, editorContent.length);
 
-        let offset = 0;
-        operations.reverse().forEach((operation, index) => {
-            var pair = operationFromMonacoChange.call(null, operation, editorContent, offset);
+        /** Base Length of First Operation must be Target Length of Second Operation */
+        expect(pair[1].targetLength).toEqual(pair[0].baseLength);
 
-            /** Base Length of First Operation must be Target Length of Second Operation */
-            expect(pair[1].targetLength).toEqual(pair[0].baseLength);
-
-            /** Base Length of Second Operation must be Target Length of First Operation */
-            expect(pair[0].targetLength).toEqual(pair[1].baseLength);
-
-            /** Correct Operations Returned */
-            expect(pair[0]).toEqual(textOperations[index]);
-
-            /** Update Offset */
-            offset += pair[0].targetLength - pair[0].baseLength;
-        });
+        /** Base Length of Second Operation must be Target Length of First Operation */
+        expect(pair[0].targetLength).toEqual(pair[1].baseLength);
+        /** Correct Operations Returned */
+        expect(pair[0]).toEqual(new firepad.TextOperation().retain(0).insert('/* ').retain(302).insert(' */'));
     });
 });
